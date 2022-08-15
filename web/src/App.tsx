6c2b4d23db1333997ab074ebb1d9bc7c4c1cb977
Paulo Vitor <paulovitor.unb@gmail.com>
2022-08-15 20:10:37 -0300

    Update page to show list of tasks and implement dialog box to insert new task in the database

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,115 @@
-import React from "react";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    Grid,
+    TextField,
+    Snackbar,
+} from "@mui/material";
+
+import { useIndex } from "./data/hooks/useIndex";
+import List from "./ui/components/list/list";
 
 function App() {
+    const {
+        tasksList,
+        task,
+        setTask,
+        newTask,
+        setNewTask,
+        title,
+        setTitle,
+        description,
+        setDescription,
+        duration,
+        setDuration,
+        datetime,
+        setDatetime,
+        message,
+        setMessage,
+        insertTask,
+        resetTaskForm,
+    } = useIndex();
+
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <>
+            <Button
+                variant={"contained"}
+                onClick={() => {
+                    setNewTask(true);
+                }}
+            >
+                Inserir Tarefa
+            </Button>
+            <List tasks={tasksList} onSelect={(task) => setTask(task)} />
+            <Dialog
+                open={newTask}
+                PaperProps={{ sx: { p: 5 } }}
+                onClose={() => {
+                    setNewTask(false);
+                    resetTaskForm();
+                }}
+            >
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField
+                            label={"Título"}
+                            type="text"
+                            variant="standard"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        ></TextField>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label={"Descrição"}
+                            type="text"
+                            variant="standard"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        ></TextField>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label={"Duração"}
+                            type="number"
+                            variant="standard"
+                            value={duration}
+                            onChange={(e) => setDuration(e.target.value)}
+                        ></TextField>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Data e hora"
+                            type="datetime-local"
+                            InputLabelProps={{ shrink: true }}
+                            variant="standard"
+                            value={datetime}
+                            onChange={(e) => setDatetime(e.target.value)}
+                        ></TextField>
+                    </Grid>
+                </Grid>
+                <DialogActions sx={{ mt: 5 }}>
+                    <Button
+                        onClick={() => {
+                            setNewTask(false);
+                            resetTaskForm();
+                        }}
+                    >
+                        Cancelar
+                    </Button>
+                    <Button variant={"contained"} onClick={() => insertTask()}>
+                        Inserir Tarefa
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Snackbar
+                open={message.length > 0}
+                message={message}
+                autoHideDuration={3000}
+                onClose={() => setMessage("")}
+            />
+        </>
     );
 }
 
