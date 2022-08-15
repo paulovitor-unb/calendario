62787beb8aca747073723812909abe215fea5cdb
Paulo Vitor <paulovitor.unb@gmail.com>
2022-08-15 12:39:14 -0300

    Show host:port on console when running the server

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,7 @@
 import { server } from "./setup.js";
 
 server.listen(process.env.API_PORT, process.env.HOST, () => {
-    console.log("API server running!");
+    console.log(
+        `API server running at http://${process.env.HOST}:${process.env.API_PORT}`
+    );
 });
